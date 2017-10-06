954ce30a009accdbfdc09a57f07905452c064b5b
Ryan Smith <0ryansmith1994@gmail.com>
2017-10-06 11:44:43 +0100

    style(lint): Fixes linting errors.

--- src/expressPresenter/mixins/json.ts
+++ src/expressPresenter/mixins/json.ts
@@ -4,7 +4,7 @@ import { RequestHandler } from 'express';
 export default (limit: string): RequestHandler => {
   return bodyParser.json({
     limit,
-    type: 'application/json',
     strict: false,
+    type: 'application/json',
   });
 };
