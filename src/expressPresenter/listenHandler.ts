b77484ced757d57cb5b997ff13ec8f70d353c489
Ryan Smith <3045513+ryansmith94@users.noreply.github.com>
2017-10-27 21:18:59 +0100

    feat(express): Adds the common listen handler.

---
+++ src/expressPresenter/listenHandler.ts
@@ -0,0 +1,23 @@
+import { LoggerInstance } from 'winston';
+
+const handleExit = (event: string) => {
+  return (error?: any) => {
+    if (error !== undefined) {
+      logger.error(error.stack);
+    }
+    logger.info(event);
+    process.exit();
+  };
+};
+
+export default (logger) => {
+  logger.info(`Listening on port ${config.express.port}`);
+  if (process.send !== undefined) {
+    logger.info('Process ready');
+    process.send('ready');
+  }
+  process.on('exit', handleExit('exit'));
+  process.on('SIGINT', handleExit('SIGINT'));
+  process.on('SIGTERM', handleExit('SIGTERM'));
+  process.on('uncaughtException', handleExit('uncaughtException'));
+};
