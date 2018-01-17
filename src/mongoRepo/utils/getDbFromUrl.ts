874486738d011fead17c719e3a13b4b32ea15d80
Ryan Smith <0ryansmith1994@gmail.com>
2018-01-17 14:59:09 +0000

    feat(mongo): Adds getDbFromUrl util.

---
+++ src/mongoRepo/utils/getDbFromUrl.ts
@@ -0,0 +1,10 @@
+// tslint:disable-next-line:max-line-length
+const regex = /^mongodb:\/\/(?:(?:[\-\w\d\%]+:[\-\w\d\%]+@)?[\-\w\d\%\.]+(?::\d+),?)*?\/([\-\w\d\%]+)/i;
+
+export default (url: string): string | undefined => {
+  const result = regex.exec(url);
+  if (result === null) {
+    return undefined;
+  }
+  return result[1];
+};
