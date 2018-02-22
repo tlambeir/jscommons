ecf7eede73a58977e8b730333a4dffd2992158c4
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-22 12:51:09 +0000

    fix(newrelic): Switches to addCustomAttribute.

--- src/tracker/newrelic.ts
+++ src/tracker/newrelic.ts
@@ -3,7 +3,7 @@ import Tracker from './Tracker';
 export default async (): Promise<Tracker> => {
   const newrelic = require('newrelic'); /* tslint:disable-line:no-require-imports */
   const tracker: Tracker = (name, value) => {
-    newrelic.addCustomParameter(name, value);
+    newrelic.addCustomAttribute(name, value);
   };
   return tracker;
 };
